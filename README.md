524dd162ef870ac3a9216c9a49f327ba160cbd21
biscoito <biscoito@riseup.net>
2023-02-26 04:34:54 -0300

    updated README

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # rustadventure
-PwnAdventure3 fun using Rust, this project goal is to have a performatic and error-safe project.
+
+PwnAdventure3 cheat made in Rust, this project's goal is to be a performatic and error-safe project.
 
 What has been implemented so far:
 
 - Detour hooking
-    - ClientWorld::Tick
-    - GameAPI::Tick
-    - Player::CanJump
-    - Player::Tick
-    - send (ws2_32.dll)
+  - ClientWorld::Tick (GameLogic.dll)
+  - GameAPI::Tick (GameLogic.dll)
+  - Player::CanJump (GameLogic.dll)
+  - Player::Tick (GameLogic.dll)
+  - send (ws2_32.dll)
 
+- Cheats
+  - Jump cooldown reset
+
+## Credits
+
+Credit where credit is due
+
+- hazedumper's [memory lib](https://github.com/frk1/hazedumper-rs)
+- [retour-rs](https://github.com/Hpmason/retour-rs): a fork of detour-rs that works on latest nightly
+- [dll-syringe](https://github.com/OpenByteDev/dll-syringe): a great-looking dll injection library for Rust
+- my friends thank you
--- cheat/src/hooks/ws_send.rs
+++ cheat/src/hooks/ws_send.rs
@@ -15,17 +15,17 @@ fn parse_buffer(buffer: &[u8]) -> Result<WebSocketPlayerInfo> {
         return Err(color_eyre::eyre::eyre!("Buffer length is not 22 bytes!"));
     }
 
-    let (identifier_byte1, identifier_byte2) = buffer.split_at(2);
-    let (positionx_byte1, positionx_byte2) = identifier_byte2.split_at(2);
-    let (positionx_byte3, positionx_byte4) = positionx_byte2.split_at(2);
-    let (positiony_byte1, positiony_byte2) = positionx_byte4.split_at(2);
-    let (positiony_byte3, positiony_byte4) = positiony_byte2.split_at(2);
-    let (positionz_byte1, positionz_byte2) = positiony_byte4.split_at(2);
-    let (positionz_byte3, positionz_byte4) = positionz_byte2.split_at(2);
-    let (rotationx_byte1, rotationx_byte2) = positionz_byte4.split_at(2);
-    let (rotationy_byte1, rotationy_byte2) = rotationx_byte2.split_at(2);
-    let (rotationz_byte1, rotationz_byte2) = rotationy_byte2.split_at(2);
-    let (forwardfrac_byte1, forwardfrac_byte2) = rotationz_byte2.split_at(2);
+    // let (identifier_byte1, identifier_byte2) = buffer.split_at(2);
+    // let (positionx_byte1, positionx_byte2) = identifier_byte2.split_at(2);
+    // let (positionx_byte3, positionx_byte4) = positionx_byte2.split_at(2);
+    // let (positiony_byte1, positiony_byte2) = positionx_byte4.split_at(2);
+    // let (positiony_byte3, positiony_byte4) = positiony_byte2.split_at(2);
+    // let (positionz_byte1, positionz_byte2) = positiony_byte4.split_at(2);
+    // let (positionz_byte3, positionz_byte4) = positionz_byte2.split_at(2);
+    // let (rotationx_byte1, rotationx_byte2) = positionz_byte4.split_at(2);
+    // let (rotationy_byte1, rotationy_byte2) = rotationx_byte2.split_at(2);
+    // let (rotationz_byte1, rotationz_byte2) = rotationy_byte2.split_at(2);
+    // let (forwardfrac_byte1, forwardfrac_byte2) = rotationz_byte2.split_at(2);
 
     Ok(WebSocketPlayerInfo {
         identifier_bytes: Vec::new(),
